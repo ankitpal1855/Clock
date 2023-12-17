8d887a232ee4cb020b4220174754e12458746fed
ankitpal1855 <154039596+ankitpal1855@users.noreply.github.com>
2023-12-17 15:22:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 setInterval(() => {
+  //Getting Time in Hours, Minutes, and Seconds//
   d = new Date();
   htime = d.getHours();
   mtime = d.getMinutes();
   stime = d.getSeconds();
 
-  htime = htime < 10 ? "0" + htime : htime; // Add leading zero if the hour is a single digit
-  mtime = mtime < 10 ? "0" + mtime : mtime; // Add leading zero if the minute is a single digit
-  stime = stime < 10 ? "0" + stime : stime; // Add leading zero if the second is a single digit
-
+  //    Adding a Zero to make Two digits Time    //
+  htime = htime < 10 ? "0" + htime : htime;
+  mtime = mtime < 10 ? "0" + mtime : mtime;
+  stime = stime < 10 ? "0" + stime : stime;
+  
+  // Making 12 hour Format//
+  htime = (htime > 12) ? "0" + (htime - 12) : htime;
+  
+// Defining Rotation of Clock Hands//
   hrotation = 30*htime + mtime/2;
   mrotation = 6 * mtime;
   srotation = 6 * stime;
-  
+
+//Rotating Clock Hands//
   hour.style.transform = `rotate(${hrotation}deg)`;
   minute.style.transform = `rotate(${mrotation}deg)`;
   second.style.transform = `rotate(${srotation}deg)`;
 
+//Digital Clock//
   total.innerHTML = `${htime}:${mtime}:${stime}`;
 }, 1000);
